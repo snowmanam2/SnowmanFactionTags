4670a246a17d6e4e4e76ae5d4a6cdb2c791690fc
Bill Smith <snowmanam2@gmail.com>
2020-01-15 22:16:29 -0600

    Add healthbar. Fix Vault dependency

--- src/main/java/com/gmail/snowmanam2/factiontags/FactionTags.java
+++ src/main/java/com/gmail/snowmanam2/factiontags/FactionTags.java
@@ -1,12 +1,16 @@
 package com.gmail.snowmanam2.factiontags;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Scoreboard;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
@@ -25,6 +29,7 @@ public class FactionTags extends JavaPlugin implements Listener {
 		config.addDefault("playersPerTick", 20);
 		config.addDefault("header", "&bHeader");
 		config.addDefault("footer", "&cFooter");
+		config.addDefault("healthSymbol", "&c❤");
 		config.options().copyDefaults(true);
 		saveConfig();
 		
@@ -41,8 +46,15 @@ public class FactionTags extends JavaPlugin implements Listener {
 		registerTeamPacketListener();
 		TeamManager.get();
 		
+		registerHealthBar();
+		
 		getServer().getPluginManager().registerEvents(this, this);
 		
+	}
+	
+	@Override
+	public void onDisable() {
+		unregisterHealthBar();
 	}
 	
 	@EventHandler
@@ -60,6 +72,28 @@ public class FactionTags extends JavaPlugin implements Listener {
 		
 	public void registerTeamPacketListener() {
 		ProtocolLibrary.getProtocolManager().addPacketListener(new TagPacketAdapter(this, PacketType.Play.Server.SCOREBOARD_TEAM));
+	}
+	
+	private void registerHealthBar() {
+		Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
+		
+		if (sb.getObjective("health") != null) {
+			sb.getObjective("health").unregister();
+		}
+		
+		Objective o = sb.registerNewObjective("health", "health");
+
+		String displayName = ChatColor.translateAlternateColorCodes('&', getConfig().getString("healthSymbol"));
+		
+		o.setDisplayName(displayName);
+		o.setDisplaySlot(DisplaySlot.BELOW_NAME);
+	}
+	
+	private void unregisterHealthBar() {
+		Scoreboard sb = Bukkit.getScoreboardManager().getMainScoreboard();
+		if (sb.getObjective("health") != null) {
+			sb.getObjective("health").unregister();
+		}
 	}
 	
 };
